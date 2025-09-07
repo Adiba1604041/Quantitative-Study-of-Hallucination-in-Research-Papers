28cd680937473df84d277fe17b06fe8603a9b41f
Adiba Ibnat Hossain <48577921+Adiba1604041@users.noreply.github.com>
2025-09-07 14:49:52 -0500

    Update 1c_Data Preprocessing.py

--- Step 1_Data Collection/1c_Data Preprocessing.py
+++ Step 1_Data Collection/1c_Data Preprocessing.py
@@ -3,7 +3,7 @@ import os
 import nltk
 from nltk.tokenize import sent_tokenize
 
-df = pd.read_csv(r"/XXXX/2025/acl2025.csv",encoding="ISO-8859-1")
+df = pd.read_csv(r"/XXXX/2024/acl2024.csv",encoding="ISO-8859-1")
 
 ## Drop duplicates
 df_unique = df.drop_duplicates()
@@ -19,12 +19,12 @@ def count_sentences(paragraph):
 # Filter rows where paragraph has 3 or more sentences (Need proper paragraphs for context)
 df_filtered = df_unique[df_unique['Paragraph'].apply(count_sentences) >= 3]
 
-folder_path = "/XXXX/ACL/CSVs/2025"
+folder_path = "/XXXX/ACL/CSVs/2024"
 
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)  
 
-csv_path = os.path.join(folder_path, "Filtered_acl2025.csv")
+csv_path = os.path.join(folder_path, "Filtered_acl2024.csv")
 
 df_filtered.to_csv(csv_path, index=False)
 
